9c1e946e1197e0f50b98833f5b6b6b245feefee9
rajesh kakumani <rajeshkakumani978@gmail.com>
2025-12-20 16:07:53 -0500

    Docker deployement

--- msapps/jobms/src/main/java/com/app/jobms/clients/CompanyClient.java
+++ msapps/jobms/src/main/java/com/app/jobms/clients/CompanyClient.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "COMPANY-SERVICE")
+@FeignClient(name = "COMPANY-SERVICE", url="${company-service.url}")
 public interface CompanyClient {
 
     @GetMapping("/companies/{companyId}")
--- msapps/jobms/src/main/java/com/app/jobms/clients/ReviewClient.java
+++ msapps/jobms/src/main/java/com/app/jobms/clients/ReviewClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "REVIEW-SERVICE")
+@FeignClient(name = "REVIEW-SERVICE", url="${review-service.url}")
 public interface ReviewClient {
 
     @GetMapping("/reviews")
